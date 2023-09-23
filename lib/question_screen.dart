eee6b58143deb88631785ff6928c46223e092a71
urdu-programming <muhammadumairali525@gmail.com>
2023-09-23 11:31:07 +0500

    fixed with of answer buttons

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/model/quiz_question.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,15 +13,40 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreen extends State<QuestionScreen> {
+  final QuizQuestion currenQuestion = questions[0];
   @override
   Widget build(context) {
-    return const Center(
-      child: Text(
-        "Hi",
-        style: TextStyle(
-          color: Color.fromARGB(255, 255, 255, 255),
-          fontSize: 30,
-        ),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currenQuestion.question,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: currenQuestion.answers[0],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currenQuestion.answers[1],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currenQuestion.answers[2],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currenQuestion.answers[3],
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
