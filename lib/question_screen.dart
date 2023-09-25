190c8f239d995a1c499a7c70d0bf4fe3ef946d5b
urdu-programming <muhammadumairali525@gmail.com>
2023-09-25 10:05:55 +0500

    adding google fonts

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/model/quiz_question.dart';
@@ -13,9 +14,18 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreen extends State<QuestionScreen> {
-  final QuizQuestion currenQuestion = questions[0];
+  int currenQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currenQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
+    QuizQuestion currenQuestion = questions[currenQuestionIndex];
+
     return Container(
       margin: const EdgeInsets.all(40),
       child: Column(
@@ -24,8 +34,10 @@ class _QuestionScreen extends State<QuestionScreen> {
         children: [
           Text(
             currenQuestion.question,
-            style: const TextStyle(
-              color: Colors.white,
+            style: GoogleFonts.croissantOne(
+              color: const Color.fromARGB(255, 221, 159, 255),
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
             ),
             textAlign: TextAlign.center,
           ),
@@ -33,7 +45,7 @@ class _QuestionScreen extends State<QuestionScreen> {
             height: 30,
           ),
           ...currenQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
+            return AnswerButton(answerText: answer, onTap: answerQuestion);
           })
         ],
       ),
